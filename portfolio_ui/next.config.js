1a565bc3c60e21861fdc2da2dc73460854225d79
aouichou <123899222+Agrippa2023@users.noreply.github.com>
2025-02-23 19:52:09 +0100

    Update Next.js configuration: change backend hostname to 'aouichou.me' and update service references for API and media paths

--- portfolio_ui/next.config.js
+++ portfolio_ui/next.config.js
@@ -2,36 +2,35 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'backend',
-		port: '8080',
-        pathname: '/media/**',
-      },
-    ],
-    minimumCacheTTL: 60,
-  },
-	async rewrites() {
-	return [
+	output: 'standalone',
+	images: {
+	  remotePatterns: [
 		{
-		source: '/api/:path*',
-		destination: 'http://backend:8080/api/:path*',
+		  protocol: 'http',
+		  hostname: 'aouichou.me',
+		  pathname: '/media/**',
 		},
-		{
-		source: '/media/:path*',
-		destination: 'http://backend:8080/media/:path*',
-		},
-	];
+	  ],
+	  minimumCacheTTL: 60,
 	},
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  }
-};
-
-module.exports = nextConfig;
+	async rewrites() {
+	  return [
+		{
+		  source: '/api/:path*',
+		  destination: 'http://backend-service:8080/api/:path*',
+		},
+		{
+		  source: '/media/:path*',
+		  destination: 'http://backend-service:8080/media/:path*',
+		},
+	  ];
+	},
+	typescript: {
+	  ignoreBuildErrors: true,
+	},
+	eslint: {
+	  ignoreDuringBuilds: true,
+	},
+  };
+  
+  module.exports = nextConfig;
