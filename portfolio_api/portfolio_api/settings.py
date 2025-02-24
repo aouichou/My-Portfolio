0961a6fd409196f707b41c70796ff1fa30133c4e
aouichou <123899222+Agrippa2023@users.noreply.github.com>
2025-02-24 15:17:27 +0100

    Remove production debug settings and clear allowed hosts in settings.py

--- portfolio_api/portfolio_api/settings.py
+++ portfolio_api/portfolio_api/settings.py
@@ -18,9 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-DEBUG = False
-ALLOWED_HOSTS = ['aouichou.me', 'www.aouichou.me']
+ALLOWED_HOSTS = []
 
 CACHES = {
     'default': {
