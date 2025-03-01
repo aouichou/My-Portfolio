c1b5f2ceb3b6ca3db7cf1b08ff60536c253a507d
aouichou <123899222+Agrippa2023@users.noreply.github.com>
2025-03-01 19:43:25 +0100

    Separate PostgreSQL service configuration and add new health check endpoint

--- portfolio_api/portfolio_api/urls.py
+++ portfolio_api/portfolio_api/urls.py
@@ -5,9 +5,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .healthcheck import health_check
+from django.http import HttpResponse
+
+def healthcheck(request):
+    return HttpResponse(status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
 	path('healthz', health_check),
+    path('healthz', healthcheck),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
