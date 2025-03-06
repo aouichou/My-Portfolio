a62c5af34e5f2fac296ddd17ebcd77b3a7381153
aouichou <123899222+Agrippa2023@users.noreply.github.com>
2025-03-06 16:00:23 +0100

    Enhance security by updating Content Security Policy and adding additional security headers in Next.js configuration

--- portfolio_ui/next.config.js
+++ portfolio_ui/next.config.js
@@ -1,39 +1,58 @@
 // portfolio_ui/next.config.ts
 
-const securityHeaders = require('./security-headers');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-	  unoptimized: true,
-	  remotePatterns: [
-		{
-		  protocol: 'https',
-		  hostname: 'aouichou.me',
-		  pathname: '/media/**',
-		},
-	  ],
-	  minimumCacheTTL: 60,
-	},
-	typescript: {
-	  ignoreBuildErrors: true,
-	},
-	eslint: {
-	  ignoreDuringBuilds: true,
-	},
-	async headers() {
-		return [
-		  {
-			source: '/security-headers.js',
-			headers: securityHeaders,
-		  },
-		];
-	  },
-	env: {
-	PORT: process.env.PORT || '3000',
-	NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '/api',
-	NEXT_PUBLIC_MEDIA_URL: process.env.NEXT_PUBLIC_MEDIA_URL || '/media',
-	},
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'aouichou.me',
+        pathname: '/media/**',
+      },
+    ],
+    minimumCacheTTL: 60,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https: http:; font-src 'self' data: https:; connect-src 'self' https: http:; media-src 'self' data: blob: https: http:; object-src 'none'; frame-src 'self';"
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          }
+        ]
+      }
+    ];
+  },
+  env: {
+    PORT: process.env.PORT || '3000',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '/api',
+    NEXT_PUBLIC_MEDIA_URL: process.env.NEXT_PUBLIC_MEDIA_URL || '/media',
+  },
 };
-  
-  module.exports = nextConfig;
+
+module.exports = nextConfig;
