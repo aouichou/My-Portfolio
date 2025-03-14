d758e11f03ecb1e8f91e92b418de24527205c020
aouichou <aouichou@student.42.fr>
2025-03-14 21:38:20 +0100

    Enhance security headers in Next.js configuration; define Content-Security-Policy and inline security headers

--- portfolio_ui/next.config.js
+++ portfolio_ui/next.config.js
@@ -1,6 +1,6 @@
 // portfolio_ui/next.config.ts
 
-const securityHeaders = require('./security-headers');
+// const securityHeaders = require('./security-headers');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,25 +19,56 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Only include essential headers here
   async headers() {
-    return [
-		{
-			source: '/:path*',
-			headers: securityHeaders
-		},
+    // Define Content-Security-Policy
+    const ContentSecurityPolicy = `
+      default-src 'self';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' data: blob: https: http:;
+      font-src 'self' data: https:;
+      connect-src 'self' https: http:;
+      media-src 'self' data: blob: https: http:;
+      object-src 'none';
+      frame-src 'self';
+    `;
+
+    // Define all security headers inline
+    const securityHeaders = [
       {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
-          }
-        ]
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+      },
+      {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+      },
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload',
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN'
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+      },
+      {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block'
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'same-origin'
+      }
+    ];
+    
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders
       }
     ];
   },
