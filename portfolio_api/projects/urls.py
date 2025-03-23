1b882a7dd532d7b5a273460b6d964e8ee56452c3
aouichou <aouichou@student.42.fr>
2025-03-23 18:24:02 +0100

    Update project URLs to use ProjectViewSet for improved list handling

--- portfolio_api/projects/urls.py
+++ portfolio_api/projects/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.debug_view, name='debug'),
-    path('projects/', views.ProjectList.as_view(), name='project-list'),
+    path('projects/', views.ProjectViewSet.as_view({'get': 'list'}), name='project-list'),
     path('projects/<slug:slug>/', views.ProjectDetail.as_view(), name='project-detail'),
     path('contact/', views.ContactSubmissionView.as_view(), name='contact-submission'),
 	path('projects/<slug:slug>/files/', views.project_files, name='project-files'),
